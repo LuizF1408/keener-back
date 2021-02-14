3f0ae1351e44c6c386d52ebe211dea9ac5c254d0
Luiz Fernando <liko.12@hotmail.com>
2021-02-14 12:56:31 -0300

    deploy

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,7 @@ if (!process.env.NODE_ENV) {
     production: {
       client: "postgresql",
       connection: process.env.DATABASE_URL,
+      ssl:true,
       pool: {
         min: 2,
         max: 10,
